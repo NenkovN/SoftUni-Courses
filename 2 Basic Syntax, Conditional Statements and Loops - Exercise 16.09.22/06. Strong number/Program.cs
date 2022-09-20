742b20e1b1814c433d36e761b0e1b9fc2dd60b9f
NenkovN <114009492+NenkovN@users.noreply.github.com>
2022-09-20 21:04:14 +0300

    add

---
+++ 2 Basic Syntax, Conditional Statements and Loops - Exercise 16.09.22/06. Strong number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int numCoppy = n;
+            int factorialSum = 0;
+
+            while (numCoppy > 0)
+            {
+                int lastDigit = numCoppy % 10;
+                numCoppy /= 10;
+
+                int factpral = 1;
+
+                for (int i = 2; i <= lastDigit; i++)
+                {
+                    factpral *= i;
+                }
+                factorialSum += factpral;
+
+            }
+
+            if (factorialSum == n) 
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+
+        }
+    }
+}
---
+++ 3 Basic Syntax, Conditional Statements and Loops - More Exercise DONE/04. Reverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            string word2 = string.Empty;
+
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                word2 += word[i];
+            }
+            Console.WriteLine(word2);
+        }
+    }
+}
