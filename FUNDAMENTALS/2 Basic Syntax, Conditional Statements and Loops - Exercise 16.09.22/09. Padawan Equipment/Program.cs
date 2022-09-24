d82afb3acb67cf6865f117c6cb03ece0607f6c3b
NenkovN <114009492+NenkovN@users.noreply.github.com>
2022-09-24 21:31:17 +0300

    me

---
+++ FUNDAMENTALS/2 Basic Syntax, Conditional Statements and Loops - Exercise 16.09.22/09. Padawan Equipment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceForLightsabers = double.Parse(Console.ReadLine());
+            double priceForRobes = double.Parse(Console.ReadLine());
+            double priceForBelts = double.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+            int belts = students / 6;
+            double students1 = Math.Ceiling((students * 0.1) + students);
+
+
+
+
+
+            totalPrice = (students1 * priceForLightsabers) + ((students - belts) * priceForBelts) + (students * priceForRobes);
+
+            if (totalPrice > money)
+            {
+                Console.WriteLine($"John will need {totalPrice - money:f2}lv more.");
+            }
+            else if (totalPrice <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+        }
+    }
+}
---
+++ FUNDAMENTALS/2 Basic Syntax, Conditional Statements and Loops - Exercise 16.09.22/11. Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersN = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int capsules = int.Parse(Console.ReadLine());
+
+            double total = 0;
+            double coffieTotal = 0;
+            double count = 0;
+
+            for (int i = 1; i <= ordersN; i++)
+            {
+                count++;
+                total = ((days * capsules) * price);
+
+                Console.WriteLine($"The price for the coffee is: ${total:f2}");
+
+
+                coffieTotal += total;
+                if (count < ordersN) 
+                {
+                    price = double.Parse(Console.ReadLine());
+                    days = int.Parse(Console.ReadLine());
+                    capsules = int.Parse(Console.ReadLine());
+                }
+                
+                
+            }
+            Console.WriteLine($"Total: ${coffieTotal:f2}");
+        }
+    }
+}
